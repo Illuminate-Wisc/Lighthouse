f8f7d797cd05cc9d86467a70983c3f3b3147425a
Thomas Lesniak <tjlesniak@wisc.edu>
2023-10-30 21:26:20 -0500

    Added Living Corridor scene

--- src/scenes/main_menu/main_menu.gd
+++ src/scenes/main_menu/main_menu.gd
@@ -6,29 +6,29 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    settings.exit_button_signal.connect(_on_return_button_pressed)
+	settings.exit_button_signal.connect(_on_return_button_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-    if Input.is_action_just_pressed("ui_cancel"):
-        if main_buttons.visible == false:
-            main_buttons.visible = true
-            settings.visible = false
-        else:
-            main_buttons.visible = false
-            settings.visible = true
+	if Input.is_action_just_pressed("ui_cancel"):
+		if main_buttons.visible == false:
+			main_buttons.visible = true
+			settings.visible = false
+		else:
+			main_buttons.visible = false
+			settings.visible = true
 
 
 func _on_quit_button_pressed():
-    get_tree().quit()
+	get_tree().quit()
 
 
 # go to the settings menu
 func _on_settings_pressed():
-    main_buttons.visible = false
-    settings.visible = true
-    
+	main_buttons.visible = false
+	settings.visible = true
+	
 func _on_return_button_pressed():
-    main_buttons.visible = true
-    settings.visible = false
+	main_buttons.visible = true
+	settings.visible = false

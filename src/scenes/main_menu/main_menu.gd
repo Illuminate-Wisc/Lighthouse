4491dc67a589cc9f41ecfff9bba6a2e355804bef
Zelong Jiang <zjiang287@wisc.edu>
2023-11-19 20:33:09 -0600

    modify some files

--- src/scenes/main_menu/main_menu.gd
+++ src/scenes/main_menu/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 @export var start_scene = "res://src/scenes/world/world.tscn"
 
-@onready var main_buttons = $MainButtons as Control
+@onready var main_buttons_container = $MainButtons as Control
 @onready var settings = $Settings as Settings
 
 
@@ -14,28 +14,28 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-    if Input.is_action_just_pressed("ui_cancel"):
-        if main_buttons_container.visible == false:
-            main_buttons_container.visible = true
-        else:
-            main_buttons_container.visible = false
+	if Input.is_action_just_pressed("ui_cancel"):
+		if main_buttons_container.visible == false:
+			main_buttons_container.visible = true
+		else:
+			main_buttons_container.visible = false
 
 
 # go to the settings menu
 func _on_settings_pressed():
-    main_buttons_container.visible = false
-    settings.visible = true
+	main_buttons_container.visible = false
+	settings.visible = true
 
 
 func _on_return_button_pressed():
-    main_buttons_container.visible = true
-    settings.visible = false
+	main_buttons_container.visible = true
+	settings.visible = false
 
 
 func _on_start_pressed():
-    SceneSwitcher.to(start_scene)
+	SceneSwitcher.to(start_scene)
 
 
 func _on_quit_pressed():
-    get_tree().quit()
+	get_tree().quit()
 

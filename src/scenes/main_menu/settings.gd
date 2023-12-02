019b707c53f385c90bef1457e665eabfe28787e8
Huong Thien Do <tdo7@wisc.edu>
2023-12-02 11:45:31 -0600

    Add the dark mode in ui

--- src/scenes/main_menu/settings.gd
+++ src/scenes/main_menu/settings.gd
@@ -4,45 +4,38 @@ class_name Settings
 signal opened
 signal closed
 
-
 var main_theme := preload("res://src/ui/themes/main/main_theme.tres")
 var user_text_bg := preload("res://src/ui/themes/main/panel/user_text_background_panel.tres")
 
 @onready var font_size_slider := $PanelContainer/PaddedContainer/SettingsContainer/FontSizeSlider
-@onready var font_color_picker := $PanelContainer/PaddedContainer/SettingsContainer/GridContainer/FontColorPicker
-@onready var background_color_picker := $PanelContainer/PaddedContainer/SettingsContainer/GridContainer/BackgroundColorPicker
 @onready var font_size_readout := $PanelContainer/PaddedContainer/SettingsContainer/FontSizeReadout
-
+@onready var dark_mode_option_button := $PanelContainer/PaddedContainer/SettingsContainer/GridContainer/DarkModeOptionButton as OptionButton
 
 func _ready():
-    font_size_slider.value = main_theme.get_font_size("font_size", "UserTextFont")
-    font_color_picker.color = main_theme.get_color("font_color", "UserTextFont")
-    background_color_picker.color = user_text_bg.bg_color
-
+	font_size_slider.value = main_theme.get_font_size("font_size", "UserTextFont")
+	dark_mode_option_button.selected = 0  # Default to dark mode 'On'
 
 func _process(_delta):
-    if Input.is_action_just_pressed("ui_cancel"):
-        if visible:
-            visible = false
-            closed.emit()
-        else:
-            visible = true
-            opened.emit()
-
+	if Input.is_action_just_pressed("ui_cancel"):
+		if visible:
+			visible = false
+			closed.emit()
+		else:
+			visible = true
+			opened.emit()
 
 func _on_exit_button_pressed():
-    visible = false
-    closed.emit()
-
+	visible = false
+	closed.emit()
 
 func _on_font_size_slider_value_changed(value):
-    main_theme.set_font_size("font_size", "UserTextFont", value)
-    font_size_readout.text = str(value) + " px"
+	main_theme.set_font_size("font_size", "UserTextFont", value)
+	font_size_readout.text = str(value) + " px"
 
-
-func _on_font_color_picker_color_changed(color):
-    main_theme.set_color("font_color", "UserTextFont", color)
-
-
-func _on_background_color_picker_color_changed(color):
-    user_text_bg.bg_color = color
+func _on_dark_mode_option_button_item_selected(index):
+	if index == 0:  # Dark mode
+		main_theme.set_color("font_color", "UserTextFont", Color(1, 1, 1))  # White font
+		user_text_bg.bg_color = Color(0.2, 0.2, 0.2)  # Dark gray background
+	else:  # Light mode
+		main_theme.set_color("font_color", "UserTextFont", Color(0, 0, 0))  # Black font
+		user_text_bg.bg_color = Color(1, 1, 1)  # White background

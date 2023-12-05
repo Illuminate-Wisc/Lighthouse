cf500919cc90779d9daf99c14650b9be275851dc
Zelong Jiang <zjiang287@wisc.edu>
2023-12-04 18:29:11 -0600

    change audio loading

--- src/sounds/SoundPlayer.gd
+++ src/sounds/SoundPlayer.gd
@@ -4,19 +4,11 @@ extends Node
 @onready var audio_player_ding = $AudioStreamDing as AudioStreamPlayer
 @onready var audio_player_stairs = $AudioStreamStairs as AudioStreamPlayer
 
-var ding = preload("res://src/sounds/ding-36029.mp3")
-var doorSound = preload("res://src/sounds/squeaky-door-open-113212.mp3")
-var stairSound = preload("res://src/sounds/metal-stairs-foot-steps-29688.mp3")
 
 
 var sounds = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#audio_player.stream = ding
-	#audio_player.play()
-	audio_player_door.stream = doorSound
-	audio_player_ding.stream = ding
-	audio_player_stairs.stream = stairSound
 	sounds = {
 		"DingSound": audio_player_ding,
 		"DoorSound": audio_player_door,
